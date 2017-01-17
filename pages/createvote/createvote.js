7a0055e20cc5dab5b1f02f03a3a25d663ff5ddc9
gogobody <138950128@qq.com>
2017-01-17 22:54:00 +0800

    second commit

--- pages/createvote/createvote.js
+++ pages/createvote/createvote.js
@@ -1,24 +1,48 @@
 // pages/createvote/createvote.js
-var utils = require("../../utils/util")
+var utils = require("../../utils/util");
+var app=getApp(); 
+
 Page({
-  data:{
-    title:"",
-    options:[],
-    date:"2016-01-01",
-    time:"13:35"
+  data: {
+    title: "",
+    options: [],
+    date: "",
+    time: "",
+    newOption: ""
   },
-changetitle:function(e){
-  this.setData({title:e.detail.value});
-},
-onLoad:function(e){
-  var time =utils.formatTime(new Date());
-  var date = utils.formatDate(new Date());
-  this.setData({date:date,time:time})
-},
-bindDateChange:function(e){
-  this.setData({date:e.detail.value})
-},
-bindTimeChange:function(e){
-  this.setData({time:e.detail.value})
-}
+  changetitle: function (e) {
+    this.setData({ title: e.detail.value });
+  },
+  onLoad: function (e) {
+    var time = utils.formatTime(new Date());
+    var date = utils.formatDate(new Date());
+    this.setData({ date: date, time: time })
+  },
+  bindDateChange: function (e) {
+    this.setData({ date: e.detail.value })
+  },
+  bindTimeChange: function (e) {
+    this.setData({ time: e.detail.value })
+  },
+  newoption: function (e) {
+    this.setData({ newOption: e.detail.value })
+  },
+  addoption: function () {
+    var tmp = this.data.options;
+    if (this.data.newOption !== "") {
+      tmp.push(this.data.newOption);
+      this.setData({ options: tmp, newOption: "" });
+    }
+  },
+  delopt: function (e) {
+    var opt = e.target.dataset.index;
+    var opts = this.data.options;
+    opts.splice(opt, 1);
+    this.setData({ options: opts })
+  },
+  pushVote: function (e) {
+    console.log("sssssssss");
+    
+  }
+
 })
